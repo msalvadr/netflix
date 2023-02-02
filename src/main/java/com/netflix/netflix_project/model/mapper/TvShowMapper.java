76b4fd3046daecc0f41e4d420fba23a1d39a7c71
Miguel A. Salvador <[msalvadr@emeal.nttdata.com]>
2023-02-02 11:56:50 +0100

    hotfix---> deleted old dependencies

--- src/main/java/com/netflix/netflix_project/model/mapper/TvShowMapper.java
+++ src/main/java/com/netflix/netflix_project/model/mapper/TvShowMapper.java
@@ -1,13 +1,11 @@
 package com.netflix.netflix_project.model.mapper;
 
 
-import com.netflix.netflix_project.model.dto.SeasonDTO;
+
 import com.netflix.netflix_project.model.dto.TvShowDTO;
-import com.netflix.netflix_project.model.entities.Season;
 import com.netflix.netflix_project.model.entities.TvShow;
 import org.mapstruct.Mapper;
 
-import java.util.List;
 
 @Mapper
 public interface TvShowMapper {
