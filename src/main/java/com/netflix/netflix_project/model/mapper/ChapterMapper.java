eba3df98a1c24065c6e7f9dc2a826f8abc105906
Miguel A. Salvador <[msalvadr@emeal.nttdata.com]>
2023-02-02 15:42:49 +0100

    merging miguel into develop

--- src/main/java/com/netflix/netflix_project/model/mapper/ChapterMapper.java
+++ src/main/java/com/netflix/netflix_project/model/mapper/ChapterMapper.java
@@ -5,7 +5,6 @@ import com.netflix.netflix_project.model.dto.ChapterDTO;
 import com.netflix.netflix_project.model.entities.Chapter;
 import org.mapstruct.Mapper;
 
-import java.util.List;
 
 @Mapper
 public interface ChapterMapper {
