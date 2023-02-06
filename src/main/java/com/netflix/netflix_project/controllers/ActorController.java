d89ca38fd5107f227e7176724a17ec0e873dc3ca
Miguel A. Salvador <[msalvadr@emeal.nttdata.com]>
2023-02-06 12:44:40 +0100

    GET and DELETE methods fully functional

--- src/main/java/com/netflix/netflix_project/controllers/ActorController.java
+++ src/main/java/com/netflix/netflix_project/controllers/ActorController.java
@@ -4,22 +4,31 @@ import com.netflix.netflix_project.model.dto.ActorDTO;
 import com.netflix.netflix_project.model.entities.Actor;
 import com.netflix.netflix_project.model.service.ActorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-
+@RequestMapping("/actors")
 public class ActorController {
 
     @Autowired
     private ActorService service;
 
-    @GetMapping(value = "/actors")
+    @GetMapping(value = "/all", produces = "application/json")
     public ResponseEntity<List<ActorDTO>> listActors(){
         List<ActorDTO> actorDTOList = service.listActors();
         return ResponseEntity.ok().body(actorDTOList);
     }
+
+
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        service.deleteActor(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,6 @@
-insert into actors (description, name) values ('guapo y listo', 'antonio');
+INSERT INTO actors (description, name)
+VALUES ('Conocido por su carisma y energía en la pantalla, ha protagonizado muchas películas de acción de gran éxito.', 'Tom Cruise'),
+       ('Considerada una de las mejores actrices de su generación, ha ganado tres premios Oscar por sus interpretaciones.', 'Meryl Streep'),
+       ('Con un estilo único y una amplia variedad de papeles, es uno de los actores más versátiles y reconocidos de Hollywood.', 'Johnny Depp'),
+       ('Ganador de un premio Oscar, es conocido por sus papeles en películas como "Fight Club" y "Once Upon a Time in Hollywood".', 'Brad Pitt'),
+       ('Conocida por su actuación cómica en películas como "Miss Congeniality" y "The Proposal", también ha demostrado su talento en papeles dramáticos.', 'Sandra Bullock');
